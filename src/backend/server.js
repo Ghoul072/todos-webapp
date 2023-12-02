ff4f9b157172a7785c92a684de3c1241d90ce8a9
Yaseen <ahm3d.yaseen@gmail.com>
2023-12-02 19:09:09 +0500

    add routes to main

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const app = express();
+
 const User = require('./models/user');
 const Todo = require('./models/todo');
+
+const userRoutes = require('./routes/userRoutes');
+const todoRoutes = require('./routes/todoRoutes');
+
 const port = 5000;
 
 app.use(express.json());
 
-User.sync({ alter: true }) // alter = true for development
+User.sync() // alter = true for development
     .then(() => {console.log('User model synced with database')})
     .catch((err) => {console.log(`Error syncing user model: ${err}`)});
 
-Todo.sync({ alter: true }) // alter = true for development
+Todo.sync() // alter = true for development
     .then(() => {console.log('ToDo model synced with database')})
     .catch((err) => {console.log(`Error syncing ToDo model: ${err}`)});
 
-
-app.get('/', (req, res) => {
-    res.send("test");
-});
-
+app.use('/users', userRoutes);
+app.use('/todos', todoRoutes);
 
 app.listen(port, (error) => {
     if (!error) {
