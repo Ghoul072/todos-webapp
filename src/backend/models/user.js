1bb71a2b76551275abab96fe26c0aac6b8a1498a
Yaseen <ahm3d.yaseen@gmail.com>
2023-12-02 13:11:58 +0500

    create user model

---
+++ src/backend/models/user.js
@@ -0,0 +1,61 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/mysql');
+const bcrypt = require('bcrypt');
+
+const User = sequelize.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {isEmail: true},
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+        validate: {
+            len: [8,100],
+            isComplexPassword(value) {
+                if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])/.test(value)) {
+                    throw new Error('Password must contain at least one numeric, one simple and one capital letter, and one special character')
+                }
+            }
+        }
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+}, {
+    timestamps: false,
+    hooks: {
+        beforeCreate: async user => {
+            const hashedPassword = await bcrypt.hash(user.password, 10);
+            user.password = hashedPassword;
+        },
+        beforeUpdate: async user => {
+            if (user.changed('password')) {
+                const hashedPassword = await bcrypt.hash(user.password, 10);
+                user.password = hashedPassword;
+            }
+        }
+    }
+});
+
+module.exports = User;
