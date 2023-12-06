b145433f96390da01e7b41eb9f1dc8d5e68cfeb5
Yaseen <ahm3d.yaseen@gmail.com>
2023-12-06 10:55:16 +0500

    add token model

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const User = require('./models/user');
 const Todo = require('./models/todo');
+const Token = require('./models/token');
 
 const userRoutes = require('./routes/userRoutes');
 const todoRoutes = require('./routes/todoRoutes');
@@ -11,13 +12,17 @@ const port = 5000;
 
 app.use(express.json());
 
-User.sync() // alter = true for development
+User.sync(alter=true) // alter = true for development
     .then(() => {console.log('User model synced with database')})
     .catch((err) => {console.log(`Error syncing user model: ${err}`)});
 
-Todo.sync() // alter = true for development
+Todo.sync(alter=true) // alter = true for development
     .then(() => {console.log('ToDo model synced with database')})
     .catch((err) => {console.log(`Error syncing ToDo model: ${err}`)});
+
+Token.sync(alter=true) // alter = true for development
+    .then(() => {console.log('Token model synced with database')})
+    .catch((err) => {console.log(`Error syncing Token model: ${err}`)});
 
 app.use('/users', userRoutes);
 app.use('/todos', todoRoutes);
