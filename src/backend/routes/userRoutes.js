7e67592e464c88735374a3ef9988ec5401e40da4
Yaseen <ahm3d.yaseen@gmail.com>
2023-12-06 10:55:52 +0500

    token functions

--- src/backend/routes/userRoutes.js
+++ src/backend/routes/userRoutes.js
@@ -1,13 +1,43 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../models/user');
+const Token = require('../models/token');
+
+const auth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
     res.send(await User.findAll());
 });
 
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: {email} });
+        if (!user) { return res.status(404).json('User not found') };
+
+        const passMatch = await bcrypt.compare(password, user.password);
+        if (!passMatch) { return res.status(401).json('Invalid Password') };
+
+        const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {'expiresIn': '15m'});
+        const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_REFRESH_SECRET, {'expiresIn': '1h'});
+
+        const token = await Token.findOne({ where: { userId: user.id } });
+        if (token) {
+            await token.update({ accessToken, refreshToken });
+        }
+        else {
+            await Token.create({ accessToken, refreshToken, 'userId': user.id });
+        }
+
+        res.status(200).json({ accessToken, refreshToken });
+
+    } catch { res.status(500).json({ 'error': 'Server Error' }) };
+});
+
+router.post('/register', async (req, res) => {
     try {
         const newUser = await User.create(req.body);
         res.status(201).json(newUser);
@@ -23,6 +53,30 @@ router.get('/:userId', async (req, res) => {
     } else {
         res.send(foundUser);
     };
+});
+
+router.post('/refresh', async (req, res) => {
+    try {
+        const token = req.body.refreshToken;
+        const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+        const userId = decoded.userId;
+
+        const user = await User.findByPk(userId);
+        if (!user) { return res.status(401).json({ 'error': 'Unauthorized - Invalid token' }) };
+
+        const accessToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {'expiresIn': '15m'});
+        const refreshToken = jwt.sign({ userId: user.id }, process.env.JWT_REFRESH_SECRET, {'expiresIn': '1h'});
+
+        const existingToken = await Token.findOne({ where: { userId: user.id } });
+        if (existingToken) {
+            await existingToken.update({ accessToken, refreshToken });
+        }
+        else {
+            await Token.create({ accessToken, refreshToken, 'userId': user.id });
+        }
+
+        res.status(200).json({ accessToken, refreshToken });
+    } catch { res.status(500).json({ 'error': 'Server Error' }) };
 });
 
 router.put('/:userId', async (req, res) => {
